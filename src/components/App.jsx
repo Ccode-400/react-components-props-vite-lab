ecf10b59d810a6fe13b65e2dc5b9284146f60b79
Ccode-400 <stanleymngige@gmail.com>
2026-05-04 09:14:27 +0300

    added default date

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,9 @@
-import React from "react";
-import blogData from "../data/blog";
-
-console.log(blogData);
-
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header name={blog.name} />
+      <About image={blog.image} about={blog.about} />
+      <ArticleList posts={blog.posts} />
     </div>
   );
 }
-
-export default App;
