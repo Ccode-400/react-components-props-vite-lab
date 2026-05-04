a2f98cd539f4db759df2754e5a9cc0959e2341a4
Ccode-400 <stanleymngige@gmail.com>
2026-05-04 08:47:57 +0300

    test: rendering blog name

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,3 +1,9 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
+test("renders blog name", () => {
+  render(<App />);
+  const title = screen.getByText(/Tanskie's Blog/i);
+  expect(title).toBeInTheDocument();
+});
+
